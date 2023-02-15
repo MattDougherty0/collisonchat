1e776f27762eac75b2bffaa4ba9c2fc8ddb3c604
MattDougherty0 <49167559+MattDougherty0@users.noreply.github.com>
2023-02-15 00:34:50 -0500

    Add files via upload

---
+++ chat3.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, render_template
+from flask_ngrok import run_with_ngrok
+from langchain.chains import llm
+from ast import Index
+from gpt_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+os.environ["OPENAI_API_KEY"] = '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'
+
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 256
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=num_outputs))
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTSimpleVectorIndex(
+      documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper, verbose=True
+      
+  )
+    
+    index.save_to_disk('index.json')
+
+    return index
+
+book_path = input("Enter the path to the directory containing your documents: ")
+index = construct_index(book_path)
+
+# Initialize the history list
+history = []
+
+@app.route('/', methods=['GET', 'POST'])
+def chatbot():
+    if request.method == 'POST':
+        query = request.form['query']
+        best_answer = index.query(query, response_mode="compact", verbose=True)
+        
+        # Add the current query and response to the history list
+        history.append((query, best_answer))
+        
+        # Reverse the order of the history list so that the most recent queries are at the top
+        history.reverse()
+        
+        return render_template('index.html', answer=best_answer, history=history)
+    
+    # Reverse the order of the history list so that the most recent queries are at the top
+    history.reverse()
+    
+    return render_template('index.html', history=history)
+
+if __name__ == '__main__':
+    app.run()
